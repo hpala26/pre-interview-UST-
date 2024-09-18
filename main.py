bf9b4a999d2924b62b619014d1bdf504f97e8f25
hpala26 <harshapala26@gmail.com>
2024-09-18 21:17:10 +0530

    reading input from a separate text file

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from src.validate_passwords import validate_passwords
 
 def main():
-    input_passwords = "as@1,a15#,2w3E*,33Au@45"
-    result = validate_passwords(input_passwords)
-    print("Valid passwords:", result)
+    input_file_path = 'src/input/passwords.txt'
+
+    with open(input_file_path, 'r') as file:
+        password_lines = file.readlines()
+
+    for idx, passwords in enumerate(password_lines):
+        passwords = passwords.strip()
+        if passwords:
+            result = validate_passwords(passwords)
+            print(f"Valid passwords from line {idx + 1}: {result}")
+
 
 if __name__ == "__main__":
     main()
